a124e82380bb3281a36efc3e5fa69b37b850bbf7
MasterDoo <masterdu9951@icloud.com>
2019-06-16 01:15:30 -0400

    fix bugs

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--initialize", type=bool, help="initialize dataset")
     parser.add_argument("-e", "--embedding", type=str, help="embedding file directory")
+    parser.add_argument("-b", "--bank", type=str, help="directory for word bank")
     parser.add_argument("--vocab", type=str, help="What vocab bank to build.")
     args = parser.parse_args()
     
@@ -29,15 +30,18 @@ if __name__ == "__main__":
         assert os.path.exists(args.embedding), f"Embedding path not found: {args.embedding}"
         print(
             f"Initialize with word2vec embedding: {args.embedding}")
-        match_map = vocab.build_best_match_map()
-        with open("./match_map.json", "w") as f:
+        match_map = vocab.build_best_match_map(
+                vocab_path=args.bank,
+                embedding_path=args.embedding
+            )
+        with open("./database/match_map.json", "w") as f:
             f.write(json.dumps(match_map))
-        print("Match map initialized, stored to ./match_map.json")
+        print("Match map initialized, stored to ./database/match_map.json")
 
 
     if args.vocab.lower() == "gre":
         start = input("Choose an arbitrary vocab to start >>> ")
         vocab = vocab.load_vocab("./database/gre3000.xlsx")
-        with open("./match_map.json", "w") as f:
+        with open("./database/match_map.json", "w") as f:
             match_map = json.loads(f)
         quiz(start, vocab, match_map)
